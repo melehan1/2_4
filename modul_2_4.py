68029bff4c48124fba03fbc0481fe78a1bff43e6
Дмитрий <melehan@mail.ru>
2024-05-30 14:02:37 +0300

    Update modul_2_4.py
    Вписал is_prime в код и вывел через нее!

--- modul_2_4.py
+++ modul_2_4.py
@@ -1,14 +1,15 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-is_prime = True
 for i in numbers:
+    is_prime = True
     if i > 1:
         for j in range(2, int(i / 2) + 1):
             if i % j == 0:
                 is_prime = False
-                not_primes.append(i)
                 break
+        if not is_prime:
+            not_primes.append(i)
         else:
             primes.append(i)
 print('Primes:', primes)
